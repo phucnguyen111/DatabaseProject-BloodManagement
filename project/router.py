d404f5fc963660baec1cc2ddc065db3bbbe1fb5c
hongeinh <honganh3179@gmail.com>
2020-04-28 23:16:36 +0700

    change router

--- project/router.py
+++ project/router.py
@@ -3,25 +3,18 @@ from aiohttp import web
 import json
 
 class Router:
-    #@get_request_json
-    async def get_users(self, request, request_json):
+    async def get_users(self, request):
         return web.Response(text="get_user")
 
-    #@get_request_json
-    async def update_users(self, request, request_json):
+    async def update_users(self, request):
         return web.Response(text="get_user")
 
-    #@get_request_json
-    async def create_user(self, request, request_json):
+    async def create_user(self, request):
         return web.Response(text="get_user")
 
-    #@get_request_json
-    async def login(self, request, request_json):
+    async def login(self, request):
         return web.Response(text="get_user")
 
-    #@get_request_json
-    async def check_login(self, request, request_json):
-        username = request_json.get('username')
-        email = request_json.get('email')
-        login = "Hello,  {} of email {}".format(username, email)
-        web.Response(text=login)
+
+    async def check_login(self, request):
+        return web.Response(text="get_user")
