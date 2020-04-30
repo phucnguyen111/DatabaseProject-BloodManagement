5a541c043f8aed104fd4c7223e6652fe7d425cd6
hongeinh <honganh3179@gmail.com>
2020-04-30 22:06:18 +0700

    add db query to create_user

--- project/db.py
+++ project/db.py
@@ -1,4 +1,5 @@
 import aiopg.sa
+import psycopg2
 
 #__all__ = ['Donor', 'Blood', 'Hospital', 'BloodBank', 'Donor_Donate_Blood',
 #           'Blood_Belongs_To_BloodBank', 'BloodBank_ManageInfo_Donor', 'BloodBank_DistributeBlood_Hospital']
@@ -19,3 +20,57 @@ async def init_pg(app):
 async def close_pg(app):
     app['db'].close()
     await app['db'].wait_closed()
+
+def create_donor(name, gender, address, email, contact, bloodgroup, medicalRecord):
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      password="ha3171999",
+                                      host="localhost",
+                                      port="5432",
+                                      database="BloodBank")
+        cursor = connection.cursor()
+
+
+        print("Adding donor: ",name,"~"+gender,"~",address,"~",email,"~",contact,"~",bloodgroup,"~",medicalRecord)
+
+        sql_insert_query = """insert into Donor(Name, Gender, Address, Email, ContactNumber, BloodGroup, MedicalReport) values(%s,%s,%s,%s,%s,%s,%s)"""
+        record_to_insert = (name, gender, address, email, contact, bloodgroup, medicalRecord)
+        cursor.execute(sql_insert_query, record_to_insert)
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "records successfully inserted into Donor table...")
+        
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error inserting record to Donor:", error)
+    finally:
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+
+def delete_donor(donID):
+    try:
+        connection = psycopg2.connect(user="postgres",
+                                      password="ha3171999",
+                                      host="localhost",
+                                      port="5432",
+                                      database="BloodBank")
+        cursor = connection.cursor()
+
+        print("Deleting donor with the ID: ", donID)
+
+        sql_insert_query = """delete from Donor where DonorID = %s"""
+        cursor.execute(sql_insert_query, (donID,))
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "records successfully deleted from Donor table...")
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error deleting record from Donor:", error)
+    finally:
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
