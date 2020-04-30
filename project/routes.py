60333044b507c857971e067037c4a1bd69ec4191
hongeinh <honganh3179@gmail.com>
2020-04-30 22:48:30 +0700

    workaround routing in html file (add route in routes.py)

--- project/routes.py
+++ project/routes.py
@@ -3,8 +3,12 @@ from router import Router
 
 def setup_routes(app):
     app.router.add_get("/", index)
+    app.router.add_get("/index.html", index)
     app.router.add_get("/register", register)
+    app.router.add_get("/register.html", register)
     app.router.add_get("/request", request)
+    app.router.add_get("/request.html", request)
+
     router_instance = Router()
     app.router.add_post('/login', router_instance.login)
     app.router.add_post('/create_user', router_instance.create_user)
