c8451db736d993e523ed69d701360a21df0bcf8a
nvhwork <nvhwork@nvhplay.tv>
2020-05-16 09:11:03 +0700

    Data entry for Donor and Blood

---
+++ DonorBloodData.sql
@@ -0,0 +1,15 @@
+INSERT INTO Donor
+	(Name, PersonalID, Gender, Address, Email, ContactNumber) VALUES
+	('Nguyen Ngoc Khanh', '087794287423', 'Female', 'Dong Da, Ha Noi', 'khanh99@gmail.com', '0983668562'),
+	('Le Trong Khoi', '005488265749', 'Male', 'Bac Tu Liem, Ha Noi', 'ltkhoi@outlook.com', '0539774873'),
+	('Do Tuyet Dung', '095428824589', 'Female', 'Vu Thu, Thai Binh', 'dung234@gmail.com', '0876344298'),
+	('Tran Tuan Nam', '001883058334', 'Male', 'Do Son, Hai Phong', 'nam.trantuan.9231@gmail.com', '0328819883'),
+	('Pham Khanh Linh', '023795748255', 'Female', 'Chi Linh, Hai Duong', 'linhlinh@outlook.com', '0913678432');
+
+INSERT INTO Blood
+	(Amount, Status, DonationDate) VALUES
+	(250, 'Normal', '23/05/2019'),
+	(350, 'Normal', '12/07/2019'),
+	(350, 'Normal', '08/08/2019'),
+	(450, 'Normal', '31/10/2019'),
+	(250, 'Normal', '02/01/2020');
