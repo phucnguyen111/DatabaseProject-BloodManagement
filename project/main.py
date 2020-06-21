636b0cebc79cd9ec7f2b615f88db6d5aed338d49
hongeinh <honganh3179@gmail.com>
2020-06-21 10:51:04 +0700

    minor

--- project/main.py
+++ project/main.py
@@ -12,6 +12,7 @@ app = web.Application()
 app['config'] = config
 
 aiohttp_jinja2.setup(app,loader=jinja2.FileSystemLoader(str(BASE_DIR / 'project' / 'templates')))
+
 setup_routes(app)
 setup_static_routes(app)
 app.on_startup.append(init_pg)
