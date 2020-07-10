0d827a44b755f87561f932430ad20c8ddac60f21
hongeinh <honganh3179@gmail.com>
2020-07-10 12:23:07 +0700

    add query to get total blood amount, modify api for getting blood amount

---
+++ project/DBQueriesPython/getBloodAmount.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+from DBQueriesPython.databaseInfo import user, password, host, port, database
+
+def getBloodAmount(bloodType):
+    try:
+        connection = psycopg2.connect(user=user,
+                                      password=password,
+                                      host=host,
+                                      port=port,
+                                      database=database)
+        cursor = connection.cursor()
+
+        cursor.execute("""select TotalAmount from BloodGroup where BloodType = '{0}'""".format(bloodType))
+        print('---executed')
+        amount = cursor.fetchone()[0]
+        return amount
+
+    except (Exception, psycopg2.Error) as error:
+        if (connection):
+            print("Error extracting statistics", error)
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
